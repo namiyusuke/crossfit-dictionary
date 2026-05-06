cc5e9218c66fd1cd2dec262b35a42902438f909c
namikawayusuke <yu.namikawa18@gmail.com>
2026-05-07 00:42:24 +0900

    faf

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import { movements } from "@/data/movements";
 import { wods } from "@/data/wods";
 import CrossFitDictionary from "@/components/CrossFitDictionary";
 
 export default function Page() {
-  return <CrossFitDictionary movements={movements} wods={wods} />;
+  return (
+    <Suspense>
+      <CrossFitDictionary movements={movements} wods={wods} />
+    </Suspense>
+  );
 }
